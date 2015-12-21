8e76eda73c12cbe012de5c36fec971a31f6ea4bb
Sergey Sadovoi <serg.sadovoi@gmail.com>
2015-12-22 00:36:13 +0200

    Yandex template fix

--- src/templates/yandex/components/Offer.js
+++ src/templates/yandex/components/Offer.js
@@ -3,8 +3,13 @@ import React, { Component, PropTypes } from 'react';
 export default class Offer extends Component {
     static propTypes = {
         id: PropTypes.number.isRequired,
-        name: PropTypes.string.isRequired,
-        price: PropTypes.number.isRequired,
+        name: PropTypes.shape({
+            short: PropTypes.string.isRequired,
+            full: PropTypes.string.isRequired
+        }).isRequired,
+        price: PropTypes.shape({
+            current: PropTypes.number.isRequired
+        }).isRequired,
         currencyId: PropTypes.string,
         available: PropTypes.bool
     }
@@ -15,11 +20,13 @@ export default class Offer extends Component {
     }
 
     render() {
+        const { id, available, name, price, currencyId } = this.props;
+
         return (
-            <offer id={this.props.id} available={this.props.available}>
-                <name>{this.props.name}</name>
-                <price>{this.props.price}</price>
-                <currencyId>{this.props.currencyId}</currencyId>
+            <offer id={ id } available={ available }>
+                <name>{ name.full }</name>
+                <price>{ price.current }</price>
+                <currencyId>{currencyId }</currencyId>
             </offer>
         );
     }
