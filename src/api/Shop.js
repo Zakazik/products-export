44b3e5e8ea03c722a68c9f557206b10d00b3fa23
Sergey Sadovoi <serg.sadovoi@gmail.com>
2015-12-22 00:36:25 +0200

    Async shop api

--- src/api/Shop.js
+++ src/api/Shop.js
@@ -14,81 +14,70 @@ import Config from 'config';
 export default class Shop {
     /**
      * Get all products
-     *
-     * @returns {Promise}
      */
-    static getProducts() {
-        return new Promise((resolve, reject) => {
-            const query = 'SELECT p.*, c.pro_cat_title_one, c.pro_cat_title' +
-                          '  FROM products p' +
-                          '  LEFT JOIN pro_categories c ON p.pro_cat_id = c.id' +
-                          '  WHERE p.trash = "N"';
-            const self = this;
+    static async getProducts() {
+        const query = 'SELECT p.*, c.pro_cat_title_one, c.pro_cat_title' +
+                      '  FROM products p' +
+                      '  LEFT JOIN pro_categories c ON p.pro_cat_id = c.id' +
+                      '  WHERE p.trash = "N"';
+        const products = await db.query(query);
 
-            db.query(query).then(async function(products){
-                let result = [];
-                for (let product of products) {
-                    let title = product.pro_cat_title_one + ' ' + product.pro_title;
-                    let url   = Config.get('shop.params.productUrlTpl').replace('{code}', product.pro_url);
-                    let image = Config.get('shop.params.imageUrlTpl').replace('{file}', product.pro_sml_image);
+        let result = [];
+        for (let product of products) {
+            let title = product.pro_cat_title_one + ' ' + product.pro_title;
+            let url   = Config.get('shop.params.productUrlTpl').replace('{code}', product.pro_url);
+            let image = Config.get('shop.params.imageUrlTpl').replace('{file}', product.pro_sml_image);
 
-                    try {
-                        result.push({
-                            id: product.id,
-                            code: (product.ind_id === '' || product.ind_id === '0') ? null : product.ind_id,
-                            name: {
-                                short: product.pro_title,
-                                full: title
-                            },
-                            description: {
-                                short: (product.block_heading === '') ? null : product.block_heading
-                            },
-                            url,
-                            image,
-                            price: {
-                                current: await Price.get(product.promo_price, product.currency)
-                            },
-                            category: {
-                                id: product.pro_cat_id,
-                                title: product.pro_cat_title
-                            },
-                            available: (product.mit_pro == 1),
-                            vendor: await self.getProp(Config.get('shop.params.vendorId'), product.id, product.pro_cat_id),
-                            country: await self.getProp(Config.get('shop.params.countryId'), product.id, product.pro_cat_id),
-                            warranty: self.parseWarranty(await self.getProp(Config.get('shop.params.warrantyId'), product.id, product.pro_cat_id))
-                        });
-                    } catch (error) {
-                        Logger.error(`Product "${product.id}": ${error.message}`);
-                    }
+            try {
+                result.push({
+                    id: product.id,
+                    code: (product.ind_id === '' || product.ind_id === '0') ? null : product.ind_id,
+                    name: {
+                        short: product.pro_title,
+                        full: title
+                    },
+                    description: {
+                        short: (product.block_heading === '') ? null : product.block_heading
+                    },
+                    url,
+                    image,
+                    price: {
+                        current: await Price.get(product.promo_price, product.currency)
+                    },
+                    category: {
+                        id: product.pro_cat_id,
+                        title: product.pro_cat_title
+                    },
+                    available: (product.mit_pro == 1),
+                    vendor: await this.getProp(Config.get('shop.params.vendorId'), product.id, product.pro_cat_id),
+                    country: await this.getProp(Config.get('shop.params.countryId'), product.id, product.pro_cat_id),
+                    warranty: this.parseWarranty(await this.getProp(Config.get('shop.params.warrantyId'), product.id, product.pro_cat_id))
+                });
+            } catch (error) {
+                Logger.error(`Product "${product.id}": ${error.message}`);
+            }
 
-                    if (result.length > 20) {
-                        break;
-                    }
-                }
+            if (result.length > 20) {
+                break;
+            }
+        }
 
-                resolve(result);
-            }).catch((error) => {
-                reject(error);
-            });
-        });
+        return result;
     }
 
-    static getProp(propId, productId, catId) {
-        return new Promise((resolve, reject) => {
-            const propQuery = `SELECT id FROM cat_properties WHERE pro_cat_id = ${catId} AND parent_id = ${propId}`;
+    static getCategories() {
 
-            db.queryOne(propQuery).then((prop) => {
-                return 'p' + pad(prop.id, 3);
-            }).then((propRow) => {
-                const propTable = 'pro' + pad(catId, 3);
-                const query = `SELECT d.value FROM ${propTable} pp LEFT JOIN dictionary d ON d.id = pp.${propRow} WHERE pp.product_id = ${productId}`;
-                return db.queryOne(query).then((prop) => {
-                    resolve(prop.value);
-                });
-            }).catch((error) => {
-                reject(error);
-            });
-        });
+    }
+
+    static async getProp(propId, productId, catId) {
+        const propQuery = `SELECT id FROM cat_properties WHERE pro_cat_id = ${catId} AND parent_id = ${propId}`;
+        const prop      = await db.queryOne(propQuery);
+        const propRow   = 'p' + pad(prop.id, 3);
+        const propTable = 'pro' + pad(catId, 3);
+        const query     = `SELECT d.value FROM ${propTable} pp LEFT JOIN dictionary d ON d.id = pp.${propRow} WHERE pp.product_id = ${productId}`;
+        const propValue = await db.queryOne(query);
+
+        return propValue.value;
     }
 
     static parseWarranty(raw) {
